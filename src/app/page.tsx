324bd45007402863850384873ed31a961cf97f3a
mohamedabdelraouf <mohamedabdelraouf>
2025-04-18 11:34:13 +0200

    edit the api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -82,6 +82,9 @@ const Home = () => {
                 {imageUrl}
               </a>
             </p>
+            <p>
+              
+            </p>
           </div>
         )}
       </div>
